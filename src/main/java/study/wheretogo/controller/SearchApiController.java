6a6f5a6bfd6f541b219ccf471c786c364a56ffe4
lifeNewbi <lastchallenged@gmail.com>
2023-03-01 02:07:48 +0900

    기본경로 Controller 추가

--- src/main/java/study/wheretogo/controller/SearchApiController.java
+++ src/main/java/study/wheretogo/controller/SearchApiController.java
@@ -1,6 +1,9 @@
 package study.wheretogo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,10 @@ public class SearchApiController {
 
     private final SearchApiService service;
 
+    @GetMapping("/")
+    public ResponseEntity GET_main() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
     @GetMapping("/map")
     public List<ResponseData> GET_mapCoordinate(
